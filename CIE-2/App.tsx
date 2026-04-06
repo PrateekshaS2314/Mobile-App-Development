4644a914a6a4f731ad586dc56d81feb2cb16a3cb
PrateekshaS2314 <prateeksha242@gmail.com>
2026-04-06 09:43:32 +0530

    Initialize React Native app with navigation stack

---
+++ CIE-2/App.tsx
@@ -0,0 +1,27 @@
+import 'react-native-gesture-handler';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import WelcomeScreen from "./src/screens/WelcomeScreen";
+import CreateAccount from "./src/screens/CreateAccount";
+
+export type RootStackParamList = {
+  Welcome: undefined;
+  CreateAccount: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
+        <Stack.Screen name="CreateAccount" component={CreateAccount} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
